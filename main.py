3253b61d19f1950efc959e2ef96e0585675e841a
Hayden Scott <HaydenScott573@gmail.com>
2022-02-09 21:35:41 -0700

    formatting, and TODOs

--- main.py
+++ main.py
@@ -13,26 +13,27 @@ import re
 # planned output
 # Date | Place | Amount | Category | Notes
 
+# TODO - loop through statements in statements/ folder
 with open('freedom_statement.csv') as sapphire:
     with open('auto_budget.csv', 'w+') as outfile:
         reader = csv.DictReader(sapphire, delimiter=',')
         outfile_field_names = ['Date', 'Place', 'Amount', 'Category', 'Notes']
-        writer = csv.DictWriter(outfile, fieldnames = outfile_field_names) 
+        writer = csv.DictWriter(outfile, fieldnames = outfile_field_names)
         writer.writeheader()
-        for input_row in reader: 
+        for input_row in reader:
             if input_row['Type'] != 'Payment':
-                # TODO - notes logic
                 notes = "fill this in"
-                # TODO - put in map or similar matcher
+                # TODO - put in regex map or similar matcher with format [regex|options|here : matching category]
+                # [amazon|amzn|amazon marketplace : Amazon]
                 if re.match('amazon|amzn', input_row['Description'], re.IGNORECASE):
                     print(f"Amazon order from {input_row['Post Date']} for amount ${input_row['Amount']}.")
-                    # todo, comma separate for category + notes
+                    # TODO, comma separate for category + notes
                     fin_category = input("categorize as: ")
                     print(fin_category)
                 else:
                     fin_category = input_row['Category']
                 writer.writerow({'Date': input_row['Post Date'], 'Place': input_row['Description'], 'Amount': input_row['Amount'], 'Category': fin_category, 'Notes': notes})
-            else: 
+            else:
                 print(f"---\npayment posted on {input_row['Transaction Date']} for amount ${input_row['Amount']}. Not categorizing\n---")
         outfile.close
     sapphire.close
